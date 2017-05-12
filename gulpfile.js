5ae22301d289ea1a49967921144b226eff39cd61
kurudrive <kurudrive@gmail.com>
2017-05-12 18:16:37 +0900

    ファイル削除テスト

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
  
 var gulp = require('gulp');
-var runSequence = require('run-sequence');
+var runSequence = require('run-sequence'); // 同期的に処理してくれる
+var del = require('del');
  
 gulp.task('copy', function() {
     return gulp.src(
@@ -14,12 +15,21 @@ gulp.task('copy', function() {
             ],
             { base: './' }
         )
-        .pipe( gulp.dest( 'dist' ) );
+        .pipe( gulp.dest( 'dist' ) ); // distディレクトリに出力
 } );
+
+gulp.task('clean', function(cb) {
+  del(['dist', 'tmp', '**/*.log','.travis.yml'], cb);
+});
+
+gulp.task('build', ['clean'], function() {
+  // Something
+});
+
 gulp.task('build:dist',function(){
     /* ここで、CSS とか JS をコンパイルする */
 });
  
 gulp.task('dist', function(cb){
-    return runSequence( 'build:dist', 'copy', cb );
+    return runSequence( 'build:dist', 'copy', 'clean', cb );
 });
