3acfe5a09f4a4e9377f254cc7af6bf8848c03c61
kurudrive <kurudrive@gmail.com>
2017-05-13 09:17:14 +0900

    とろえ

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,6 +5,7 @@ set -e
 git clone -b dist --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git" dist
 npm run dist
 cd dist
+## すべての変更を含むワークツリーの内容をインデックスに追加.
 git add -A
 git commit -m "Update from travis $TRAVIS_COMMIT"
 git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" dist 2> /dev/null
