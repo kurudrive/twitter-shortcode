d11d2ee56ad2f556d0305c943bd5819a9fdf76c1
travis-ci <kurudrive@gmail.com>
2017-05-13 22:20:04 +0000

    Update from travis 3758b034d6cbd79e0877ac816125c260e99c565f

--- twitter-shortcode.php
+++ twitter-shortcode.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     twitter-shortcode
  * Domain Path:     /languages
- * Version:         0.1.5
+ * Version:         0.1.6
  *
  * @package         Twitter_Shortcode
  */
