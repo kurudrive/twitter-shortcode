3f860f8e2b0c4e8cab443b0881802d26e906bc36
kurudrive <kurudrive@gmail.com>
2017-05-15 16:10:46 +0900

    2回やらなあかんのか

--- twitter-shortcode.php
+++ twitter-shortcode.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     twitter-shortcode
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         0.1.0
  *
  * @package         Twitter_Shortcode
  */
