397f4fda0410e6e655b4ba76a7fbc059fd97a3a4
kurudrive <kurudrive@gmail.com>
2017-05-16 12:01:18 +0900

    test削除

--- twitter-shortcode.php
+++ twitter-shortcode.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     twitter-shortcode
  * Domain Path:     /languages
- * Version:         0.1.11
+ * Version:         0.1.12
  *
  * @package         Twitter_Shortcode
  */
