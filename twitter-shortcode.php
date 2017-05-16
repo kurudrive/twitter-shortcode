363a0e05f26fb20004f4065c0488f6b81b8bb8f8
kurudrive <kurudrive@gmail.com>
2017-05-16 11:57:30 +0900

    ゴミ削除 これも正常にいけるはず

--- twitter-shortcode.php
+++ twitter-shortcode.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     twitter-shortcode
  * Domain Path:     /languages
- * Version:         0.1.10
+ * Version:         0.1.11
  *
  * @package         Twitter_Shortcode
  */
