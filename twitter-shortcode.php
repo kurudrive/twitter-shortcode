c3bdf438ee51acf69cd47eda0ed505b4ba135145
kurudrive <kurudrive@gmail.com>
2017-05-16 11:51:24 +0900

    デプロイする？

--- twitter-shortcode.php
+++ twitter-shortcode.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     twitter-shortcode
  * Domain Path:     /languages
- * Version:         0.1.9
+ * Version:         0.1.10
  *
  * @package         Twitter_Shortcode
  */
