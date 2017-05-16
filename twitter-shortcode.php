6cf2e253a0a31d58a50f66af0166d8c0d5a967f6
kurudrive <kurudrive@gmail.com>
2017-05-16 18:27:31 +0900

    これでtestはコミットされないはず

--- twitter-shortcode.php
+++ twitter-shortcode.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     twitter-shortcode
  * Domain Path:     /languages
- * Version:         0.1.15
+ * Version:         0.1.16
  *
  * @package         Twitter_Shortcode
  */
